85f4e32d8f593ffd0aa762abef72411a64697119
piko <70123212+pikoUsername@users.noreply.github.com>
2024-03-20 12:10:20 +0600

    Update DependencyInjection.cs

--- NameApp/Domain/DependencyInjection.cs
+++ NameApp/Domain/DependencyInjection.cs
@@ -7,7 +7,11 @@ namespace NameApp.Domain
     {
         public static IServiceCollection AddDomainServices(this IServiceCollection services)
         {
-            services.AddScoped<PasswordService, PasswordService>();
+            services.AddScoped<PasswordService, PasswordService>(
+                x => {
+                    return new PasswordService(passwordHasher: new PasswordHasher<UserEntity>());
+                }
+            );
             services.AddScoped<UserEntityService, UserEntityService>();
             services.AddScoped<PermissionEntityService, PermissionEntityService>();
 
